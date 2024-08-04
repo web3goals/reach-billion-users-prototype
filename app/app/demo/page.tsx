08ee3a4f964df0068aac8345da95d0bd6c61aef1
kiv1n <aloha@kiv1n.ru>
2024-08-04 09:59:23 +0300

    Implement structure for rba context

--- app/app/demo/page.tsx
+++ app/app/demo/page.tsx
@@ -5,7 +5,7 @@ import { RBAProvider, useRBA } from "@/components/rba-provider";
 export default function DemoPage() {
   return (
     <RBAProvider>
-      <main className="container py-10 lg:px-80">
+      <main className="container py-10 lg:px-64">
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tight tracking-tighter">
           DEMO
         </h1>
@@ -18,16 +18,29 @@ export default function DemoPage() {
 }
 
 function DemoFunctions() {
-  const { tonAddress, connectTon, disconnectTon } = useRBA();
+  const { tonAddress, tonConnect, tonDisconnect, ethAddress, ethExecute } =
+    useRBA();
+
+  async function mintUsdt() {
+    const executeDestination = "0x0"; // USDT contract
+    const executeFunction = "0x0"; // Encoded function data
+    const txHash = await ethExecute?.(executeDestination, executeFunction);
+    console.log({ txHash });
+  }
 
   return (
     <div className="flex flex-col items-start gap-4">
-      <p>Ton Address: {tonAddress || "None"}</p>
-      <button className="bg-gray-800 p-4" onClick={() => connectTon?.()}>
+      <p className="text-sm">Ton Address: {tonAddress || "None"}</p>
+      <button className="bg-gray-800 p-4" onClick={() => tonConnect?.()}>
         Connect Ton
       </button>
-      <button className="bg-gray-800 p-4" onClick={() => disconnectTon?.()}>
+      <button className="bg-gray-800 p-4" onClick={() => tonDisconnect?.()}>
         Disconnect Ton
+      </button>
+      <div className="w-full h-[2px] bg-gray-800 my-4" />
+      <p className="text-sm">Eth Address: {ethAddress || "None"}</p>
+      <button className="bg-gray-800 p-4" onClick={() => mintUsdt()}>
+        Mint USDT
       </button>
     </div>
   );
--- app/components/rba-provider.tsx
+++ app/components/rba-provider.tsx
@@ -3,12 +3,17 @@ import {
   useTonAddress,
   useTonConnectUI,
 } from "@tonconnect/ui-react";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type RBAContextType = {
   tonAddress?: string;
-  connectTon?: () => Promise<void>;
-  disconnectTon?: () => Promise<void>;
+  tonConnect?: () => Promise<void>;
+  tonDisconnect?: () => Promise<void>;
+  ethAddress?: `0x${string}`;
+  ethExecute?: (
+    executeDestination: `0x${string}`,
+    executeFunction: `0x${string}`
+  ) => Promise<string>;
 };
 
 const RBAContext = createContext<RBAContextType>({});
@@ -16,13 +21,41 @@ const RBAContext = createContext<RBAContextType>({});
 function RBAProvider({ children }: { children: React.ReactNode }) {
   const [tonConnectUI] = useTonConnectUI();
   const tonAddress = useTonAddress();
+  const [ethAddress, setEthAddress] = useState<`0x${string}` | undefined>();
+
+  // TODO: Implement
+  // TODO: Use API?
+  async function ethExecute(
+    executeDestination: `0x${string}`,
+    executeFunction: `0x${string}`
+  ): Promise<string> {
+    if (!ethAddress) {
+      throw new Error("Ethereum address is not defined");
+    }
+    // Use state eth address
+    // Create fake bundler account
+    // Create init code
+    // Send user operation using fake bunder account
+    return "xyz";
+  }
+
+  // TODO: Create eth wallet and store it in browser local storage
+  useEffect(() => {
+    if (tonAddress) {
+      setEthAddress("0x0000000000000000000000000000000000000000");
+    } else {
+      setEthAddress(undefined);
+    }
+  }, [tonAddress]);
 
   return (
     <RBAContext.Provider
       value={{
         tonAddress: tonAddress,
-        connectTon: () => tonConnectUI.openModal(),
-        disconnectTon: () => tonConnectUI.disconnect(),
+        tonConnect: () => tonConnectUI.openModal(),
+        tonDisconnect: () => tonConnectUI.disconnect(),
+        ethAddress: ethAddress,
+        ethExecute: ethExecute,
       }}
     >
       {children}
