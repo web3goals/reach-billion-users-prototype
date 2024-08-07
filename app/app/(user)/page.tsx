60b3379cf6d1bea882bdde791f02df67925b425a
kiv1n <aloha@kiv1n.ru>
2024-08-07 08:41:29 +0300

    Implement landing page

--- app/app/(user)/page.tsx
+++ app/app/(user)/page.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <div className="container py-10 lg:px-80">
-      <p>Landing page...</p>
+    <div className="container flex flex-col items-center py-10 xl:px-80">
+      <h1 className="text-4xl md:text-5xl font-extrabold tracking-tighter text-center max-w-[820px]">
+        Attract a billion Telegram users to your dApp on Ethereum
+      </h1>
+      <h2 className="text-2xl font-normal tracking-tight text-center text-muted-foreground max-w-[820px] mt-4">
+        A dev kit to integrate Telegram Open Network into your dApp without
+        headaches in a few clicks
+      </h2>
+      <Link href="/dashboard">
+        <Button className="mt-6" size="lg">
+          🪄 Open Dashboard
+        </Button>
+      </Link>
+      <div className="flex flex-col items-center max-w-[640px] mt-8">
+        <Image
+          src="/images/integration.png"
+          alt="Integration"
+          priority={false}
+          width="100"
+          height="100"
+          sizes="100vw"
+          className="w-full"
+        />
+      </div>
     </div>
   );
 }
