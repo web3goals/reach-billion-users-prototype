536c34fd45bc1a7db1548e24a5bf6e17e4bafd84
kiv1n <aloha@kiv1n.ru>
2024-08-10 08:33:45 +0300

    Redeploy contracts for demo

--- app/config/csp.ts
+++ app/config/csp.ts
@@ -4,9 +4,9 @@ export const cryptoSpacePrisonConfig = {
   network: "optimismSepolia",
   chain: optimismSepolia,
   contracts: {
-    usdToken: "0x1b21550F42E993d1b692d18D79bCd783638633F2" as `0x${string}`,
-    exchanger: "0x418d621b98Cc75a09327725620F9ec949615396E" as `0x${string}`,
+    usdToken: "0xe05B06f086BE56C22A4cc70708f95d28ca9A8320" as `0x${string}`,
+    exchanger: "0x9a1C3C845BAd2585210913914Bf88242460062E9" as `0x${string}`,
     cryptoSpacePrison:
-      "0x4F316c6536Ce3ee94De802a9EfDb20484Ec4BDF9" as `0x${string}`,
+      "0x63a465e2C7Ca890430f81B7cE8Ad86bc9A4512DD" as `0x${string}`,
   },
 };
